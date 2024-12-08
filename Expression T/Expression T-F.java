dd1eea1fe9734a481c285e0e835ad225bdd14745
Tamim-29 <dsiamm49@gmail.com>
2024-12-08 12:04:02 +0600

    Delete Expression T directory

--- Expression T/Expression T-F.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-  public static void main(String[] args) {
-    int x = 10;
-    int y = 9;
-    System.out.println(x > y); // returns true, because 10 is higher than 9  
-  }
-}
